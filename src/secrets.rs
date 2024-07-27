aaa1eccc8133d01043f201f943619da8dd8f449d
daniel@emmettschmidt.com <daniel@emmettschmidt.com>
2024-07-27 16:29:13 -0400

    Better, now some indication of error source

--- src/secrets.rs
+++ src/secrets.rs
@@ -15,8 +15,13 @@ pub fn getConnString() -> String
     let basePathOption = env::current_dir().expect("Couldn't find current dir");
     let secretsPath: String = format!("{}{}", basePathOption.into_os_string().into_string().expect("OS error converting a string"), "/secrets/secrets.json");
 
-    let data = fs::read_to_string(secretsPath).unwrap();
+    let mut path = "".to_string();
+    
+    match fs::read_to_string(secretsPath){
+        Ok(val) => {path = val.clone().to_string()},
+        Err(err) => { println!("No path found at {}: {}", path, err.to_string()) }
+    };
 
-    let secrets: Secrets = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    let secrets: Secrets = serde_json::from_str(&path).expect("JSON was not well-formatted");
     return secrets.DbConn;
 }
